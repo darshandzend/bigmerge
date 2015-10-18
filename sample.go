a4a24dd41bba7dfcb9be9aefb4df12fb4d6081a1
dzen <darshandzend@gmail.com>
2015-10-18 13:17:01 +0530

    Random input in sample.go

--- sample.go
+++ sample.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 
 	"github.com/darshandzend/bigmerge/src"
@@ -11,7 +12,7 @@ func main() {
 	b := bigmerge.New()
 	ch := b.IpChan()
 	for i := 100; i > 0; i-- {
-		ch <- i //rand.Intn(100)
+		ch <- rand.Intn(100)
 		time.Sleep(time.Millisecond * 1)
 	}
 	close(ch)
